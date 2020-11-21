5b4634ed3fbcc1cf8a90ef2450e156f5b72152e0
siriwatknp <siriwatkunaporn@gmail.com>
2020-11-21 13:30:37 +0700

    feat: 🎸 useState vs useRef

--- src/examples/UseStateVsUseRef.js
+++ src/examples/UseStateVsUseRef.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Layout from "../components/Layout";
 
-const UseStateVsUseRef = () => (
-  <Layout>
-    Hello
-  </Layout>
-);
+const UseStateVsUseRef = () => {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+  return (
+    <Layout>
+      <p>
+        <code>useState</code> trigger rerendering, but <code>useRef</code> is
+        not.
+      </p>
+      <h2>
+        <code>useState</code>
+      </h2>
+      <div className="d-flex gap-4 align-items-center">
+        <div className="fs-4">count: {count}</div>
+        <button className="btn btn-dark" onClick={() => setCount((c) => c + 1)}>
+          Increase
+        </button>
+      </div>
+      <br />
+      <br />
+      <h2>
+        <code>useRef</code>
+      </h2>
+      <div className="d-flex gap-4 align-items-center">
+        <div className="fs-4">count: {countRef.current}</div>
+        <button
+          className="btn btn-dark"
+          onClick={() => {
+            countRef.current = countRef.current + 1;
+          }}
+        >
+          Increase
+        </button>
+        <button
+          className="btn btn-info"
+          onClick={() => alert(countRef.current)}
+        >
+          Show countRef
+        </button>
+      </div>
+    </Layout>
+  );
+};
 
 export default UseStateVsUseRef;
