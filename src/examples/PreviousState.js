9b653460b075c9ad9d55124465f5e969f464696e
siriwatknp <siriwatkunaporn@gmail.com>
2020-11-21 16:52:03 +0700

    feat: 🎸 add setInterval example

--- src/examples/PreviousState.js
+++ src/examples/PreviousState.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Layout from "../components/Layout";
 
-const PreviousState = () => (
-  <Layout>PreviousState</Layout>
-);
+const PreviousState = () => {
+  const [count, setCount] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current = count;
+  }, [count]);
+  return (
+    <Layout>
+      <h1>PreviousState</h1>
+      <p>Current count: {count}</p>
+      <p>
+        <button className="btn btn-dark" onClick={() => setCount(c => c + 1)}>Increase</button>
+      </p>
+      {ref.current > 0 && <p>Previous count: {ref.current}</p>}
+    </Layout>
+  );
+};
 
 export default PreviousState
--- src/examples/SetInterval.js
+++ src/examples/SetInterval.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from "react";
 import Layout from "../components/Layout";
 
-const SetInterval = () => (
-  <Layout>SetInterval</Layout>
-);
+function useInterval(callback, delay) {
+  const callbackRef = useRef(callback)
+  useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+  useEffect(() => {
+    const id = setInterval(() => callbackRef.current(), delay);
 
-export default SetInterval
+    return () => clearInterval(id);
+  }, [delay]);
+}
+
+const randomNumber = () => Math.floor(Math.random() * 1000);
+
+const SetInterval = () => {
+  const [delay, setDelay] = useState(1000);
+  const [count, setCount] = useState(0);
+  useInterval(() => {
+    console.log(count)
+    setCount(count + 2)
+  }, 1000);
+  return (
+    <Layout>
+      <h1>Interval</h1>
+      <div className="border-1 border-info border rounded p-4 mb-4">
+        <p>
+          The number will be increased by 1 every{" "}
+          <input
+            style={{ width: 80 }}
+            value={delay}
+            type="number"
+            onChange={(e) => setDelay(Number(e.target.value))}
+          />
+          ms.
+        </p>
+        <div className="fs-4">{count}</div>
+        <div className="mt-4">
+          <button
+            className="btn btn-dark"
+            onClick={() => setCount(randomNumber())}
+          >
+            Random count
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default SetInterval;
