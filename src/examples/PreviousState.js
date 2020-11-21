f0b8e0064ce6c033d36de096c85f212479ddd9c7
siriwatknp <siriwatkunaporn@gmail.com>
2020-11-21 20:04:37 +0700

    feat: 🎸 add more case to PreviousState example

--- src/examples/PreviousState.js
+++ src/examples/PreviousState.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import Layout from "../components/Layout";
 
 const PreviousState = () => {
@@ -8,16 +8,31 @@ const PreviousState = () => {
   useEffect(() => {
     ref.current = count;
   }, [count]);
+
+  const previousCount = ref.current;
   return (
     <Layout>
       <h1>PreviousState</h1>
       <p>Current count: {count}</p>
       <p>
-        <button className="btn btn-dark" onClick={() => setCount(c => c + 1)}>Increase</button>
+        <button
+          className="btn btn-dark mr-2"
+          onClick={() => setCount((c) => c + 1)}
+        >
+          Increase
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            alert(`Previous : ${previousCount}`);
+          }}
+        >
+          Show previous
+        </button>
       </p>
       {ref.current > 0 && <p>Previous count: {ref.current}</p>}
     </Layout>
   );
 };
 
-export default PreviousState
+export default PreviousState;
